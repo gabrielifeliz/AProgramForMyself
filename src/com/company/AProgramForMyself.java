826a23e1193bb72e35d5e4931353fdf0bd45a2da
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-05-31 15:05:10 -0400

    initial commit

---
+++ src/com/company/AProgramForMyself.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class AProgramForMyself {
+
+    public static void main(String[] args) {
+        // Name: Gabriel I. Feliz   Age: 18
+        int daysPassed = 3, weeksRemaining = 7;
+        String programmingLang = "Java";
+
+        System.out.println("Hello, everyone!");
+        System.out.println("This is day " + daysPassed + " of the Java Web Development Bootcamp");
+        System.out.println("We are learning basics of " + programmingLang);
+        System.out.println("I am excited to learn more about programming the next " + weeksRemaining + " weeks");
+    }
+}
